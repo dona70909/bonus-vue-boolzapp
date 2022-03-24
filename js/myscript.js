f6a62021d0f2e13cfbf655ff3505446b4a3764fd
Donatella7 <campaniellodonatella@gmail.com>
2022-03-24 14:51:47 +0100

    modifiche

--- js/myscript.js
+++ js/myscript.js
@@ -1,16 +1,13 @@
 
+/* import vue template emoij */
 Vue.use(EmojiPicker)
-
-
 const app = new Vue ({
     
     el:"#app",
 
     data:{
-        
-        input: '',
-        search: '',
 
+        search: '',
         contacts: [
             {
                 name: 'Michele',
@@ -32,6 +29,17 @@ const app = new Vue ({
                         message: 'Tutto fatto!',
                         status: 'received'
                     }
+                ],
+                
+                image:"avatar_1.jpg",
+                
+            },
+
+            {
+                name: 'senza messages',
+                avatar: '_1',
+                visible: true,
+                messages: [
                 ],
                 
                 image:"avatar_1.jpg",
@@ -198,15 +206,19 @@ const app = new Vue ({
             }
         ],
         
-        activeClick:null,
+        activeClick:0,
         inputMessage:"",
         inputContact:"",
         activeMessage:null,
         
     },
     
-    
     methods: {
+
+        /**
+         * function that get the active element after an event and check if it's equal to the current one
+         * @param {*} index index of the current element
+         */
         getActiveClick(index){
             this.activeClick = index;
         },
@@ -214,6 +226,13 @@ const app = new Vue ({
         getActiveMessage(index){
             this.activeMessage = index;
         },
+
+        /**
+         * 
+         * Function that push an element into the array of object, if it's active.
+         * 
+         * @param {*} pushElement element to push
+         */
         
         pushMessage(pushElement){
             this.contacts.forEach((contact,index) => {
@@ -225,6 +244,13 @@ const app = new Vue ({
                 }
             });
         },
+
+        /**
+         * 
+         * Function that remove an element into the array of object, if it's active.
+         * 
+         * @param {*} indexMsg reomove at index  
+         */
         
         removeMessage(indexMsg){
             this.contacts.forEach((contact,index) => {
@@ -236,19 +262,14 @@ const app = new Vue ({
                 }
             });
         },
-        
-        // ! input without answer
-        /*getInput(input){
-            if(input != ""){
-                newMessage = this.createNewMessage();
-                this.pushMessage(newMessage);
-                newMessage.message = input;
-                this.inputMessage = "";
-            } else {
-                console.log("empty msg");
-            }
-        },*/
-        
+
+        /**
+         * Function that get the input from v-model,
+         * push it to the object property
+         * 
+         * @param {*} input v-model input
+         */
+
         getInput(input){
             if(input != ""){
                 newMessage = this.createNewMessage();
@@ -261,6 +282,12 @@ const app = new Vue ({
                 console.log("empty msg");
             }
         },
+
+    
+        /**
+         * function that create a new object message
+         * @returns 
+         */
         
         createNewMessage(){
             const today = new Date();
@@ -271,7 +298,11 @@ const app = new Vue ({
                 status:"sent"
             }
         },
-        
+
+        /**
+         * function that create a new object answer
+         * @returns 
+         */
         createNewAnswer(){
             const today = new Date();
             return {
@@ -281,23 +312,49 @@ const app = new Vue ({
                 status:"received"
             }
         },
-        
+
+
+        /**
+         * timing function that push the answer obj 
+         */
         timeFn(){
             this.pushMessage(newAnswer);
         },
-        
+
+        /**
+         * function that assign to the v-model input the emoij
+         * @param {*} emoji 
+         */
         append(emoji) {
             this.inputMessage += emoji
         },
+
+        getsearch(input){
+            this.contacts.forEach((contact,index) => {
+                if(contact.name.toLowerCase().startsWith(input.toLowerCase())){
+                    contact.visible = true;
+                    return true;
+                } else {
+                    contact.visible = false;
+                    return false;
+                }
+            })
+        }
+
     },
     
     computed: {
-        checkSearchContact(){
+
+        /**
+         * Function filter the object, if the contacts array of object includes the inputContact(from the input)
+         * @returns filterd list after search
+         */
+        /*  contacts(){
             this.activeClick = 0;
             return this.contacts.filter(contact => {
-                return contact.name.toLowerCase().includes(this.inputContact.toLowerCase());
+                return contact.name.toLowerCase().startsWith(this.inputContact.toLowerCase());
             });
-        }
+        }, */
     },
     
     directives: {
